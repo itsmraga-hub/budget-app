0ca6c2454e671960607087a998971cbd7cb3374d
Raga <ragawilliam570@gmail.com>
2022-12-19 23:38:22 +0300

    Add many to many relationship to Group and Transaction

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,5 +1,7 @@
 class Expense < ApplicationRecord
-  has_many :budget_categories
+  has_many :budget_category_expenses
+
+  has_many :budget_categories, through: :budget_category_expenses
 
   belongs_to :author, class_name: 'User'
 end
