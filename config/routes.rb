327feb053f6a7595cc7d23cb0e901e87ad9ff5e7
Raga <ragawilliam570@gmail.com>
2022-12-19 14:12:58 +0300

    Modify routes to rediect to splash screen

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  devise_scope :user do
-    # Redirests signing out users back to sign-in
-    root to: "devise/sessions#new"
-  end
+  # devise_scope :user do
+    # Redirects signing out users back to sign-in
+    # root to: "devise/sessions#new"
+    # root to: 'splash_screen#index'
+  # end
 
   # devise_for :users
 
@@ -16,4 +17,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  root 'splash_screen#index'
 end
