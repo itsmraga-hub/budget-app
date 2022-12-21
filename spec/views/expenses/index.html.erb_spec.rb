23b8b9b843e709cc0e6a615ef52762797c5b56e2
Raga <ragawilliam570@gmail.com>
2022-12-21 14:01:19 +0300

    Fix rubocop errors

--- spec/views/expenses/index.html.erb_spec.rb
+++ spec/views/expenses/index.html.erb_spec.rb
@@ -8,7 +8,8 @@ RSpec.describe 'BudgetCategories#index', type: :feature do
     @budget_category2 = BudgetCategory.create!(author: user, name: 'Insurance', icon: 'https://images.unsplash.com/photo-1612831457048-9823ce1ebb6e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80')
     @budget_category3 = BudgetCategory.create!(author: user, name: 'Groceries', icon: 'https://images.unsplash.com/photo-1612831457048-9823ce1ebb6e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80')
     @transaction1 = Expense.create!(author: user, name: 'Fare', amount: 100.0, budget_categories: [@budget_category1])
-    @transaction2 = Expense.create!(author: user, name: 'Groceries', amount: 100.0, budget_categories: [@budget_category1, @budget_category2])
+    @transaction2 = Expense.create!(author: user, name: 'Groceries', amount: 100.0,
+                                    budget_categories: [@budget_category1, @budget_category2])
     @transaction3 = Expense.create!(author: user, name: 'Fare', amount: 100.0, budget_categories: [@budget_category3])
     @budget_categories = BudgetCategory.all
     @user = User.first
